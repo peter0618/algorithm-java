3870ee3c6450dbfe08ef7490d171ed1c960f0b80
peter0618 <peterchoi0618@gmail.com>
2022-08-21 16:30:09 +0900

    1152번 단어의 개수

--- baekjoon/strings/NumberOfWords.java
+++ baekjoon/strings/NumberOfWords.java
@@ -1,14 +1,20 @@
 package baekjoon.strings;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 1152번 단어의 개수
  */
 public class NumberOfWords {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextLine().trim().split(" ").length);
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] tokens = br.readLine().trim().split(" ");
+        if(tokens.length == 1 && tokens[0].equalsIgnoreCase("")) {
+            System.out.println(0);
+        } else {
+            System.out.println(tokens.length);
+        }
     }
 }
