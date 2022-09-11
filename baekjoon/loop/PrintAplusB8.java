57d973e984adadb8b740eebbf04bba813330d419
peter0618 <peterchoi0618@gmail.com>
2022-09-11 22:00:44 +0900

    11022번 A+B - 8

---
+++ baekjoon/loop/PrintAplusB8.java
@@ -0,0 +1,20 @@
+package baekjoon.loop;
+
+import java.io.*;
+
+/**
+ * 11022번 A+B - 8
+ */
+public class PrintAplusB8 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0 ; i < T ; i++) {
+            String[] numbers = br.readLine().split(" ");
+            bw.write("Case #" + (i+1) + ": " + (Integer.parseInt(numbers[0]) + " + " + Integer.parseInt(numbers[1])) + " = " + (Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1])) + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
