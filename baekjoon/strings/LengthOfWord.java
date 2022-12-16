98eb83b30e37b7da652d2e318ab7cb4a4e03cbcc
peter0618 <peterchoi0618@gmail.com>
2022-12-17 03:26:22 +0900

    2743번 단어 길이 재기

---
+++ baekjoon/strings/LengthOfWord.java
@@ -0,0 +1,15 @@
+package baekjoon.strings;
+
+import java.util.Scanner;
+
+/**
+ * 2743번 단어 길이 재기
+ */
+public class LengthOfWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.length());
+        sc.close();
+    }
+}
